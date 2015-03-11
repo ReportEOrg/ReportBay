a6ba565afdddc777a271140eff41c9b3f84c2f73
wongmk <MinKeat.Wong@centurylink.com>
2015-03-11 09:21:50 +0800

    removed unreference imports

--- report-features/report-gen/src/main/java/org/reporte/report/service/ReportGenerationService.java
+++ report-features/report-gen/src/main/java/org/reporte/report/service/ReportGenerationService.java
@@ -3,7 +3,6 @@ package org.reporte.report.service;
 import java.util.List;
 
 import org.reporte.datasource.domain.Datasource;
-import org.reporte.model.domain.Model;
 import org.reporte.report.domain.AreaChartReport;
 import org.reporte.report.domain.BarChartReport;
 import org.reporte.report.domain.ColumnChartReport;
@@ -15,8 +14,6 @@ import org.reporte.reporttemplate.domain.BarChartTemplate;
 import org.reporte.reporttemplate.domain.ColumnChartTemplate;
 import org.reporte.reporttemplate.domain.LineChartTemplate;
 import org.reporte.reporttemplate.domain.PieChartTemplate;
-import org.reporte.reporttemplate.domain.ReportQuery;
-import org.reporte.reporttemplate.service.exception.ReportTemplateServiceException;
 
 /**
  * 
