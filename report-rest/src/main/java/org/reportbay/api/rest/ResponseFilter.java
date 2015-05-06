9052b80b1f5573e56318426f8984af4fd5c575e0
wongmk <MinKeat.Wong@centurylink.com>
2015-05-06 14:27:52 +0800

    rectified the implementation to use ReponseFilter instead of WriterInterceptor

---
+++ report-rest/src/main/java/org/reportbay/api/rest/ResponseFilter.java
@@ -0,0 +1,32 @@
+package org.reportbay.api.rest;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Provider
+public class ResponseFilter implements ContainerResponseFilter{
+
+	private static final Logger LOG = LoggerFactory.getLogger(ResponseFilter.class);
+
+	@Override
+	public void filter(ContainerRequestContext requestContext,
+			ContainerResponseContext responseContext) throws IOException {
+		
+		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+		//TODO: put in proper domain
+
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");	
+		
+		LOG.debug("setting response header: {}",headers.toString());
+	}
+	
+}
