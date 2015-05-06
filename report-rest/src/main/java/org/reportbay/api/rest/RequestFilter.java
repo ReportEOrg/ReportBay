49c5d42ccbece610c627cb324bd9e3a344168b9e
Ahamed.Nijamudeen <Ahamed.Nijamudeen@SG1LS406966.savvis.ad.savvis.net>
2015-05-06 15:22:15 +0800

    Created Request Filter to filter out OPTIONS and send back 200 status

---
+++ report-rest/src/main/java/org/reportbay/api/rest/RequestFilter.java
@@ -0,0 +1,31 @@
+package org.reportbay.api.rest;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Provider
+@PreMatching
+public class RequestFilter implements ContainerRequestFilter {
+
+	private final static Logger LOG = LoggerFactory.getLogger(RequestFilter.class);
+
+    @Override
+    public void filter( ContainerRequestContext requestCtx ) throws IOException {
+        LOG.info( "Executing REST request filter for method ["+requestCtx.getRequest().getMethod()+"]" );
+        // When HttpMethod comes as OPTIONS, just acknowledge that it accepts...
+        if ( requestCtx.getRequest().getMethod().equals( "OPTIONS" ) ) {
+            LOG.info( "HTTP Method (OPTIONS) - Detected! Aborting Request with Status 200" );
+            // Just send a OK signal back to the browser
+            requestCtx.abortWith( Response.status( Response.Status.OK ).build() );
+        }
+    }
+
+}
